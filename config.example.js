d03966251bff50bb5d8df3bf4ddef7d5295b1957
Anushree <anushreevsurve@gmail.com>
2026-02-07 16:07:01 +0530

    Added config.example.js

---
+++ config.example.js
@@ -0,0 +1,10 @@
+// config.example.js
+// COPY THIS TO config.js AND FILL IN YOUR KEYS
+export const firebaseSecretConfig = {
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
+};
