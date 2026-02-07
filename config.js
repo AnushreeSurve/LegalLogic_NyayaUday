955773754112fc0fc4733c7d20d025b571391735
Anushree <anushreevsurve@gmail.com>
2026-02-07 17:48:42 +0530

    Update

--- config.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-  apiKey: "AIzaSyBA-uEZEHmqmzbkHTun7x0vFdtqQOVY718",
-  authDomain: "tejal-818e5.firebaseapp.com",
-  projectId: "tejal-818e5",
-  storageBucket: "tejal-818e5.firebasestorage.app",
-  messagingSenderId: "279100767488",
-  appId: "1:279100767488:web:ece2a0b28a7c9c0c6347e5"
-};
--- firebase.js
+++ firebase.js
@@ -2,12 +2,16 @@
 import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
-import { firebaseConfig } from "./config.js";
+const firebaseConfig = {
+  apiKey: "AIzaSyBA-uEZEHmqmzbkHTun7x0vFdtqQOVY718",
+  authDomain: "tejal-818e5.firebaseapp.com",
+  projectId: "tejal-818e5",
+  storageBucket: "tejal-818e5.firebasestorage.app",
+  messagingSenderId: "279100767488",
+  appId: "1:279100767488:web:ece2a0b28a7c9c0c6347e5"
+};
 
 const app = initializeApp(firebaseConfig);
 
 // ✅ EXPORT — do NOT attach to window
 export const firebaseDB = getFirestore(app);
-
-console.log("Config loaded:", firebaseSecretConfig);
-console.log("Firebase App initialized:", app.name);
